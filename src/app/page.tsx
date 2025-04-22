1f289053fff836247f35b0a8fda7fcfb6564271b
https-dre <diaso.andre@outlook.com>
2025-04-22 17:27:24 -0300

    add github profile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
+'use client'
 import { Button } from "@/components/ui/button";
-import { ChevronRight, Link2 } from "lucide-react";
+import { ChevronRight, Link2, Github } from "lucide-react";
+import * as React from "react";
 
 export default function Home() {
   const menu_buttons_style = "w-[100%] bg-transparent text-[15px] py-[20px] px-[10px] text-stone-400 hover:bg-stone-900"
+  const [showLink, setShowLink] = React.useState(false);
   return (
     <div className="flex justify-center h-full w-full">
       <div className="p-2  my-auto font-mono w-[400px]">
@@ -32,12 +35,29 @@ export default function Home() {
           </Button>
         </div>
         <hr />
-        <div className="flex items-center justify-between mt-[10px]">
-          <Button variant={"ghost"}>
-            <Link2 size={15} />
-          </Button>
-          <p className="text-stone-400 p-[10px] text-[12px]">v1.0</p>
-        </div>
+        {
+          showLink ? (
+            <div className="flex items-center mt-[10px]">
+              <Button 
+              variant={"ghost"}
+              onClick={() => window.open("https://github.com/https-dre", "GitHub Profile")}
+              >
+                <Github size={20} className="text-stone-400"/>
+              </Button>
+
+            </div>
+          )
+            : (
+              <div className="flex items-center justify-between mt-[10px]">
+                <Button variant={"ghost"}
+                  onClick={() => setShowLink(true)}
+                >
+                  <Link2 size={15} className="text-stone-400" />
+                </Button>
+                <p className="text-stone-400 p-[10px] text-[12px]">v1.0</p>
+              </div>
+            )
+        }
       </div>
     </div>
   );
