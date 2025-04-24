4ba74d20032b9f8c70c76a7ad3b35b67d32c7a52
https-dre <diaso.andre@outlook.com>
2025-04-24 16:42:19 -0300

    refactor: corrigir tipo de ano e ajustar layout do componente Project

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -6,32 +6,26 @@ type ProjectProps = {
     description: string | null;
     image: string | null;
     link: string | null;
-    year: string;
+    year: number;
 }
 
 const Project = (props: ProjectProps) => {
-    return <div className="flex-col font-mono border-[2px] rounded-lg">
+    return <div className="flex flex-col max-w-[400px] gap-0 font-mono border-[2px] rounded-lg">
         {
             props.image ? (
-                <img src={props.image} alt="Project Image" className="rounded-t-lg w-[100%]" />
+                <img src={props.image} alt="Project Image" className="w-[100%] h-[200px] rounded-t-lg" />
             )
-            : (
-                <Skeleton className=" w-[100%] h-[200px]" />
-            )
+                : (
+                    <Skeleton className="w-[100%] h-[200px]" />
+                )
         }
-        
-        
-        <div className="p-[20px]">
+
+        <div className="relative p-[20px] h-60  flex flex-col">
             <h2 className="mb-[15px] text-lg">{props.name}</h2>
             <p className="text-stone-400">{props.description}</p>
-
-            <div className="flex items-align justify-between mt-[20px]">
-                <p className="text-[11px] items-align bg-stone-500 px-[5px] rounded-[3px]">{props.year}</p>
-                <button>
-                    <SquareArrowOutUpRight className="text-stone-400" size={15}/>
-                </button>
-            </div>
+            <p className="absolute bottom-[20px] text-[11px] bg-stone-500 px-[5px] rounded-[3px] w-min">{props.year}</p>
         </div>
+
     </div>
 }
 
