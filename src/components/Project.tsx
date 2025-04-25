ec282635616c7ec09c80905c8b42b27039544549
https-dre <diaso.andre@outlook.com>
2025-04-25 13:34:42 -0300

    refactor: ajustar estrutura do componente Project e melhorar a manipulação de hover

--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,16 +1,23 @@
 'use client';
+import React from "react";
 import { Skeleton } from "./ui/skeleton";
-import { SquareArrowOutUpRight } from "lucide-react"
+import { ChevronRight } from "lucide-react";
+
 type ProjectProps = {
     name: string | null;
     description: string | null;
     image: string | null;
-    link: string | null;
+    link: string;
     year: number;
 }
 
 const Project = (props: ProjectProps) => {
-    return <div className="flex flex-col max-w-[400px] gap-0 font-mono border-[2px] rounded-lg">
+    const [hover, setHover] = React.useState(false);
+
+    return <div
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+        className="relative flex flex-col max-w-[400px] gap-0 font-mono border-[2px] rounded-lg">
         {
             props.image ? (
                 <img src={props.image} alt="Project Image" className="w-[100%] h-[200px] rounded-t-lg" />
@@ -20,14 +27,21 @@ const Project = (props: ProjectProps) => {
                 )
         }
 
-        <div className="relative p-[20px] h-60  flex flex-col">
+        <div className="m-[20px] h-60 flex flex-col">
             <h2 className="mb-[15px] text-lg">{props.name}</h2>
             <p className="text-stone-400">{props.description}</p>
-            <p className="absolute bottom-[20px] text-[11px] bg-stone-500 px-[5px] rounded-[3px] w-min">{props.year}</p>
         </div>
 
+        <div className="absolute bottom-[20px] px-[20px] w-full flex items-center justify-between ">
+            <p className="text-[11px] bg-amber-800 px-[5px] rounded-[3px] w-min">{props.year}</p>
+            <button data-hover={hover}
+                onClick={() => window.open(props.link)}
+                className={`flex items-center cursor-pointer transition-colors duration-300 ${hover ? "text-stone-300" : "text-transparent"}`}>
+                View
+                <ChevronRight size={15}/>
+            </button>
+        </div>
     </div>
 }
 
 export default Project;
-
